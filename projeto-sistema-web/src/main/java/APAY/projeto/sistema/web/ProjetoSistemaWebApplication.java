064f87d925ddf7e97860af1654ae6f6423581a5f
Ana Vitória <anavitoria.234234234@gmail.com>
2024-11-26 20:49:34 -0300

    Commit Inicial

---
+++ projeto-sistema-web/src/main/java/APAY/projeto/sistema/web/ProjetoSistemaWebApplication.java
@@ -0,0 +1,13 @@
+package APAY.projeto.sistema.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoSistemaWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoSistemaWebApplication.class, args);
+	}
+
+}
---
+++ projeto-sistema-web/src/test/java/APAY/projeto/sistema/web/ProjetoSistemaWebApplicationTests.java
@@ -0,0 +1,13 @@
+package APAY.projeto.sistema.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoSistemaWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
